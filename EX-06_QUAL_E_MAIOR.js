b8010b2848cb0921505a9f7a713f33e1fe44dc89
ThiagoThe <nitrox2thi@gmail.com>
2023-02-27 10:58:51 -0300

    ex_06_qual é maior

---
+++ EX-06_QUAL_E_MAIOR.js
@@ -0,0 +1,8 @@
+const num1 = parseInt(prompt("Digite um número: "));
+const num2 = parseInt(prompt("Digite outro número: "));
+
+if (num1 > num2) {
+  console.log("O número " + num1 + " é maior");
+} else {
+  console.log("O número " + num2 + " é maior");
+}
